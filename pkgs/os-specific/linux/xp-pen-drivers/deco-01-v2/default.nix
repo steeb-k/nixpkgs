98fbf5a9a75768207defeb8eb3586cdd97f55fd5
steeb-k <133536278+steeb-k@users.noreply.github.com>
2023-05-13 13:02:11 -0700

    Update default.nix
    Updated hash, removing requirement to run as root.

--- pkgs/os-specific/linux/xp-pen-drivers/deco-01-v2/default.nix
+++ pkgs/os-specific/linux/xp-pen-drivers/deco-01-v2/default.nix
@@ -25,7 +25,7 @@ stdenv.mkDerivation rec {
   src = fetchzip {
     url = "https://www.xp-pen.com/download/file/id/1936/pid/440/ext/gz.html#.tar.gz";
     name = "xp-pen-deco-01-v2-driver-${version}.tar.gz";
-    sha256 = "sha256-xrRDxH7e00dISXb+lTtrnui+fNFpX7bLke2o+aTjJNk=";
+    sha256 = "sha256-CV4ZaGCFFcfy2J0O8leYgcyzFVwJQFQJsShOv9B7jfI=;
   };
 
   nativeBuildInputs = [
